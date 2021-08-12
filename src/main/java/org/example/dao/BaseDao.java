1026cf1d5cb8742557c7c75bd0e6133ab130303e
Almanbet-Arstan <72572980+Almanbet-Arstan@users.noreply.github.com>
2021-08-12 06:42:21 +0600

    Update BaseDao.java

--- src/main/java/org/example/dao/BaseDao.java
+++ src/main/java/org/example/dao/BaseDao.java
@@ -5,7 +5,7 @@ import java.sql.*;
 public class BaseDao {
     private final String url = "jdbc:postgresql://localhost:5432/";
     private final String user = "postgres";
-    private final String password = "a1l2m3a4";
+    private final String password = "";
 
     public Connection connect() throws SQLException {
         try {
